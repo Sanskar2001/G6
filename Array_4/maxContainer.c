6c3b902fe064d132b2ae4910e60b8c20eac7bf33
Sanskar2001 <sanskaratrey22@gmail.com>
2023-01-31 08:56:19 +0530

    l4

---
+++ Array_4/maxContainer.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+	int arr[]={1,8,6,2,5,4,8,3,7};
+	int n=sizeof(arr)/sizeof(int);
+
+
+	int s=0,e=n-1;
+	int maxArea=0;
+	while(s<e)
+	{
+			int hieght= arr[s]<arr[e] ? arr[s] : arr[e];
+			int width=e-s;
+
+			int currentArea=hieght*width;
+
+			maxArea= (currentArea>maxArea) ? currentArea : maxArea;
+
+
+
+			if(arr[s]<arr[e])
+				s++;
+
+			else if(arr[s]>arr[e])
+				e--;
+
+			else
+			{
+				s++;
+				e--;
+			}
+	}
+
+
+	printf("maxArea= %d ",maxArea);
+
+
+}
