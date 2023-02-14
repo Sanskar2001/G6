c73b2b2258d1972120b44bfffd9d9b181a92f4c1
Sanskar2001 <sanskaratrey22@gmail.com>
2023-02-14 12:45:38 +0530

    added biggest No problem

---
+++ char_Array_2/biggestNo.c
@@ -0,0 +1,44 @@
+// https://hack.codingblocks.com/app/contests/3942/1086/problem
+#include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int compare(const void* a,const void* b)
+{
+    char str1[20],str2[20];
+
+    int no1= *(int*)a;
+    int no2=*(int*)b;
+
+    sprintf(str1,"%d%d", no1,no2);
+    sprintf(str2,"%d%d",no2,no1);
+ 
+	if(strcmp(str1,str2)>0)
+	return -1;
+
+	return 1;
+   
+}
+int main() {
+    int t;
+	scanf("%d",&t);
+
+	while(t--)
+	{
+	int n;
+	scanf("%d",&n);
+
+	int arr[n];
+
+	for(int i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+
+    qsort(arr,n,sizeof(int),compare);
+
+    for(int i=0;i<n;i++)
+    printf("%d",arr[i]);
+
+	printf("\n");
+	}
+
+  
+}
